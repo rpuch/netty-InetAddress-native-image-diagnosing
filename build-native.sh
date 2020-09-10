4661846a208dc41d55f61644c480ab2b9d503387
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2020-09-10 21:06:59 +0400

    Move io.netty.resolver.dns.DnsNameResolver initialization to runtime

--- build-native.sh
+++ build-native.sh
@@ -3,5 +3,6 @@
 native-image --no-server --no-fallback -H:+TraceClassInitialization -H:ConfigurationFileDirectories=target/config \
 --allow-incomplete-classpath \
 --initialize-at-build-time=io.netty \
+--initialize-at-run-time=io.netty.resolver.dns.DnsNameResolver \
 -cp `find target/deps | tr '\n' ':'`target/netty-InetAddress-native-image-diagnosing-1.0-SNAPSHOT.jar Main target/app
 
