e0d5ecf6003e83b9e5b7533ecdd87983abf72392
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2020-09-10 21:14:14 +0400

    Move io.netty.resolver.HostsFileEntriesResolver initialization to runtime

--- build-native.sh
+++ build-native.sh
@@ -4,5 +4,6 @@ native-image --no-server --no-fallback -H:+TraceClassInitialization -H:Configura
 --allow-incomplete-classpath \
 --initialize-at-build-time=io.netty \
 --initialize-at-run-time=io.netty.resolver.dns.DnsNameResolver \
+--initialize-at-run-time=io.netty.resolver.HostsFileEntriesResolver \
 -cp `find target/deps | tr '\n' ':'`target/netty-InetAddress-native-image-diagnosing-1.0-SNAPSHOT.jar Main target/app
 
